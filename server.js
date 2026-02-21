7b5f6c421dacc54c94ab9ee36229d4e886e29c39
Faustine Mniko <mnikophone@gmail.com>
2026-02-21 15:21:48 +0300

    updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
