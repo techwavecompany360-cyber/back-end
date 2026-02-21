af9ad723a3a2b8aa1fc69de43fdcf0a9670ea95f
Faustine Mniko <mnikophone@gmail.com>
2026-02-21 15:58:23 +0300

    update

--- server.js
+++ server.js
@@ -8,7 +8,27 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  "https://admin.rem360.co.tz",
+  "https://rem360.co.tz",
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(morgan("dev"));
 
